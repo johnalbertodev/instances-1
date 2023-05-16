076c4c8f4696518b1e9b90bd8d687cc9c07fa8f0
Ankit Kumar <ankitgandral007@gmail.com>
2023-05-16 09:26:58 +0000

    adding new changes

--- main.tf
+++ main.tf
@@ -6,3 +6,12 @@ key = "student.3-network-state"
 region = "us-west-2"
 }
 }
+
+resource "aws_instance" "my_vm" {
+ami = "ami-0db245b76e5c21ca1"
+subnet_id = data.terraform_remote_state.network_details.outputs.my_subnet
+instance_type = "t3.micro"
+tags = {
+Name = "student.3-vm1"
+}
+}
