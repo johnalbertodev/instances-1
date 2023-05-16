93a876e35e3d8223051ae7864cbdc6843372f847
Ankit Kumar <ankitgandral007@gmail.com>
2023-05-16 12:35:19 +0000

    adding new files

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ backend = "s3"
 config = {
 bucket = "student.3-my-new-bucket"
 key = "student.3-network-state"
-region = "us-west-2"
+region = var.region
 }
 }
 
---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "profile" {
+ default = "student.3"
+}
+
+variable "region" {
+ default = "us-west-2"
+}
+
+
