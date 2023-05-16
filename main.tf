adad872655c2ccabe4207ec14c2db7cb2b572763
Ankit Kumar <ankitgandral007@gmail.com>
2023-05-16 13:06:30 +0000

    adding new files

--- main.tf
+++ main.tf
@@ -7,12 +7,13 @@ region = var.region
 }
 }
 
-resource "aws_instance" "my_vm" {
+module "webserver" {
+source = "./modules/linux_node"
 ami = "ami-0db245b76e5c21ca1"
-subnet_id = data.terraform_remote_state.network_details.outputs.my_subnet
-key_name = data.terraform_remote_state.network_details.outputs.aws_key_pair
-vpc_security_group_ids = data.terraform_remote_state.network_details.outputs.security_group_id_array
 instance_type = "t3.micro"
+key_name = data.terraform_remote_state.network_details.outputs.aws_key_pair
+subnet_id = data.terraform_remote_state.network_details.outputs.my_subnet
+vpc_security_group_ids = data.terraform_remote_state.network_details.outputs.security_group_id_array
 tags = {
 Name = "student.3-vm1"
 }
---
+++ modules/linux_node/instances.tf
@@ -0,0 +1,10 @@
+resource "aws_instance" "my_vm" {
+ami = var.ami
+subnet_id = var.subnet_id
+key_name = var.key_name
+vpc_security_group_ids = var.vpc_security_group_ids
+instance_type = var.instance_type
+tags = var.tags
+}
+
+
